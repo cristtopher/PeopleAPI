77770ff4cc57cc94cc61c1e969843fed959bc9ce
Cristtopher Quintana T <cquintana@patagoniswings.com>
2017-06-28 11:29:15 -0400

    add dni return to getName remote hook

--- common/models/person.js
+++ common/models/person.js
@@ -7,14 +7,17 @@ module.exports = function(Person) {
       where: {dni: dni}, limit: 1},
       function(err, person) {
         if (err) { cb(null,null); }
-        cb(null, person[0].name);
+        cb(null, person[0].dni, person[0].name);
       });
   };
   Person.remoteMethod(
     'getName',
     {
       accepts: [{arg: 'dni', type: 'string'}],
-      returns: {arg: 'name', type: 'string'},
+      returns: [
+        {arg: 'dni', type: 'string'},
+        {arg: 'name', type: 'string'}
+      ],
       http: {path:'/getName', verb: 'get'}
     }
   );
