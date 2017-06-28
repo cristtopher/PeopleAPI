bf5462959c7b72b5317527a7f6cb18eab8fe0759
Cristtopher Quintana T <cquintana@patagoniswings.com>
2017-06-28 11:04:06 -0400

    filter by dni getName remote hook

--- common/models/person.js
+++ common/models/person.js
@@ -1,5 +1,21 @@
 'use strict';
 
 module.exports = function(Person) {
-
+  // remote method
+  Person.getName = function(dni, cb) {
+    Person.find({
+      where: {dni: dni}, limit: 1},
+      function(err, person) {
+        if (err) { cb(null,null); }
+        cb(null, person[0].name);
+      });
+  };
+  Person.remoteMethod(
+    'getName',
+    {
+      accepts: [{arg: 'dni', type: 'string'}],
+      returns: {arg: 'name', type: 'string'},
+      http: {path:'/getName', verb: 'get'}
+    }
+  );
 };
