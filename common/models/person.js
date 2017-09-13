f79e57165b28c8ea3ebbcb13eb8372040d73f050
Cristtopher Quintana T <cquintana@axxezo.com>
2017-09-13 11:49:30 -0300

    fix: add uniqueness of dni for people model

--- common/models/person.js
+++ common/models/person.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = function(Person) {
+  // validate uniqueness of dni
   Person.validatesUniquenessOf('dni');
   // remote method
   Person.getName = function(dni, cb) {
